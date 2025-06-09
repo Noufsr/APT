6eb8c3352655733906c17792780e78eb1e7b0431
Fabian <fab.silvar1@gmail.com>
2025-06-09 00:50:56 -0400

    jasjas

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,6 +1,7 @@
+import { FirestoreService } from 'src/app/services/firestore.service';
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../../services/auth.service';
-import { Observable } from 'rxjs';
+import { Observable, take } from 'rxjs';
 import { User } from '../../models/user.models';
 import { CajaComponent } from '../../components/caja/caja.component';
 import { AlertController, ModalController, NavController, ToastController } from '@ionic/angular';
@@ -17,6 +18,7 @@ export class HomePage implements OnInit {
     private authService: AuthService,
     private modalController: ModalController,
     private alertController: AlertController,
+    private firestoreService: FirestoreService,
   ) {}
 
   ngOnInit() {
@@ -27,70 +29,38 @@ export class HomePage implements OnInit {
         console.log('No hay usuario autenticado');
       }
     });
+
+    this.verificarApertura();
+
   }
+
+  async verificarApertura() {
+  const aperturas = await this.firestoreService.getAperturasAbiertas().pipe(take(1)).toPromise();
+  if (!aperturas || aperturas.length === 0) {
+    this.abrirModalCaja('apertura');
+  }
+}
+
+
+
+
+
+
 
   // Login
-  async login() {
-    try {
-      await this.authService.login('email@example.com', 'password');
-    } catch (error) {
-      console.error('Error en el login', error);
-    }
-  }
 
-  // Logout
-  async logout() {
-    try {
-      await this.authService.logout();
-    } catch (error) {
-      console.error('Error en el logout', error);
-    }
-  }
 
-  //apertura cierre
-  async realizarApCi() {
 
-    const alert = await this.alertController.create({
-      header: 'Apertura/Cierre',
-      message: `Que desea realizar`,
-      inputs: [
-        {
-          name: 'accion',
-          type: 'radio',
-          label: 'Apertura de caja',
-          value: 'apertura',
-          checked: true
-        },
-        {
-          name: 'accion',
-          type: 'radio',
-          label: 'Cierre de caja',
-          value: 'cierre'
-        }
-      ],
-      buttons: [
-        {
-          text: 'Cancelar',
-          role: 'cancel'
-        },
-        {
-          text: 'Confirmar',
-          handler: (accion) => {
-            this.abrirModalCaja(accion);
-          }
-        }
-      ]
-    });
-
-    await alert.present();
-  }
 
   async abrirModalCaja(accion: 'apertura' | 'cierre') {
   const modal = await this.modalController.create({
     component: CajaComponent,
     componentProps: {
-      accion: accion
-    }
+      accion: accion},
+      backdropDismiss: false,
+  showBackdrop: true,
+
+
   });
 
   return await modal.present();
